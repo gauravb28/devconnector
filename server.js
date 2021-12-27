ccb8057f75f29867cc6f04f3449d1c0bccc22b1c
Gaurav Bansal <bansalgaurav726@gmail.com>
2021-12-27 20:51:01 +0530

    build scripts

--- server.js
+++ server.js
@@ -26,5 +26,9 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
