cd13fdde95fe440c8aee6984daed311930aa4499
Gaurav Bansal <bansalgaurav726@gmail.com>
2022-07-01 19:55:48 +0530

    Path added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Initialise app
 const app = express();
